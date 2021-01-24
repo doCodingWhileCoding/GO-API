4a7c279d9ccb665fbd98d34f375fe877300275c2
Nadir <nadirboughlalajob@gmail.com>
2021-01-24 17:54:55 +0100

    24/01/2021 17:54

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("public"))
+	//Creación de un servidor web //////////////////////////////////
+	/*fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 	log.Println("Servidor iniciado en: http://127.0.0.1:8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)*/
 
+	// Handler /////////////////////////////////////
+	//Registrando la ruta y el handler
+	http.HandleFunc("/saludar", saludar)
+	//sube un servidor en el puerto 8080
+	http.ListenAndServe(":8080", nil)
+}
+
+func saludar(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo")
 }
